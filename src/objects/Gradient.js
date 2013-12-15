ef819bd805cfb0e1956d31c311f0295fbd2087ee
Guy Cook <guy@ivorydungeon.net>
2013-12-15 23:27:18 +1300

    Add Gradient and GradientStop object types

---
+++ src/objects/Gradient.js
@@ -0,0 +1,10 @@
+// QML Gradient
+QObjects.Gradient = {};
+
+Object.defineProperties(QObjects.Gradient, {
+  defaultProperties: {
+    value: {
+      stops: []
+    }
+  }
+});
---
+++ src/objects/GradientStop.js
@@ -0,0 +1,11 @@
+// QML GradientStop
+QObjects.GradientStop = {};
+
+Object.defineProperties(QObjects.GradientStop, {
+  defaultProperties: {
+    value: {
+      color: 'black',
+      position: 0
+    }
+  }
+});
