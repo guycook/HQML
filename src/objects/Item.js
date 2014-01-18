guycook/HQML

commit c739d422a93fc67550c89bbd6527f99b7692e4a4

Store position changes made by anchors so they propogate to dependent properties

src/objects/Item.js
@@ -45,6 +45,13 @@ QObjects.Item = {
         x = this.anchors.right - width - (+this.anchors.rightMargin);
       }
     }
+
+    // Update internal absolute positioning if affected by anchors
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
     node.setX(x + border / 2);
     node.setY(y + border / 2);
     node.setWidth(width - border);

commit c254a99b1c68418ce0fcdab24fecf3b3792f54de

Don't update bindings not affected by anchor layout algorithm
Fixes bug introduced in c739d422

src/objects/Item.js
@@ -10,10 +10,10 @@ QObjects.Item = {
     return { x: 0, y: 0 };
   },
   layout: function(node) {
-    var x = this.x,
-        y = this.y,
-        width = this.width,
-        height = this.height,
+    var x = this.x, startX = x,
+        y = this.y, startY = y,
+        width = this.width, startWidth = width,
+        height = this.height, startHeight = height,
         border = nullOrUndefined(this.border) ? 0 : this.border.width;
 
     if(!nullOrUndefined(this.anchors.fill)) {
@@ -83,10 +83,10 @@ QObjects.Item = {
     }
 
     // Update internal absolute positioning if affected by anchors
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+    if(x !== startX) this.x = x;
+    if(y !== startX) this.y = y;
+    if(width !== startWidth) this.width = width;
+    if(height !== startHeight) this.height = height;
 
     node.setX(x + border / 2);
     node.setY(y + border / 2);